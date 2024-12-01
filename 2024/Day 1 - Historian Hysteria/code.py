9fa90d9d591cc9ec69fff74dd4f6ccd5a218b320
DimitarMalamski <mitemalamata@gmail.com>
2024-12-01 15:53:19 +0100

    Day 1 - part 1 complete

--- 2024/Day 1 - Historian Hysteria/code.py
+++ 2024/Day 1 - Historian Hysteria/code.py
@@ -1,1 +1,22 @@
-print(1 + 1)
+list_one = []
+list_two = []
+
+with open('Day 1 - Historian Hysteria/input.txt', 'r') as file:
+   for line in file:
+      splited = line.strip().split("   ")
+      list_one.append(int(splited[0]))
+      list_two.append(int(splited[1]))
+
+distance = 0
+
+for i in range(len(list_one)):
+   number_one = min(list_one)
+   number_two = min(list_two)
+
+   distance += abs(number_one - number_two)
+
+   list_one.remove(number_one)
+   list_two.remove(number_two)
+
+print(distance)
+
